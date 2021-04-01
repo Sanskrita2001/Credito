0e8a446b6628a45c966e44403b9679f4cbece1fd
AKDev1 <60657206+AKDev1@users.noreply.github.com>
2021-04-01 14:50:52 +0530

    Add files via upload

---
+++ course.js
@@ -0,0 +1,64 @@
+function showMore1(){
+  if(document.getElementById("hid1").style.display == "none"){
+    document.getElementById("hid1").style.display = "table";
+    document.getElementById('daplus1').src = "minus.png";
+    document.getElementById('daplus1').style.paddingTop = "15px";
+  }
+  else{
+    document.getElementById("hid1").style.display = "none";
+    document.getElementById('daplus1').src = "plus.png";
+    document.getElementById('daplus1').style.paddingTop = "0";
+  }
+}
+
+function showMore2(){
+  if(document.getElementById("hid2").style.display == "none"){
+    document.getElementById("hid2").style.display = "table";
+    document.getElementById('daplus2').src = "minus.png";
+    document.getElementById('daplus2').style.paddingTop = "15px";
+  }
+  else{
+    document.getElementById("hid2").style.display = "none";
+    document.getElementById('daplus2').src = "plus.png";
+    document.getElementById('daplus2').style.paddingTop = "0";
+  }
+}
+
+function showMore3(){
+  if(document.getElementById("hid3").style.display == "none"){
+    document.getElementById("hid3").style.display = "table";
+    document.getElementById('quizplus1').src = "minus.png";
+    document.getElementById('quizplus1').style.paddingTop = "15px";
+  }
+  else{
+    document.getElementById("hid3").style.display = "none";
+    document.getElementById('quizplus1').src = "plus.png";
+    document.getElementById('quizplus1').style.paddingTop = "0";
+  }
+}
+
+function showMore4(){
+  if(document.getElementById("hid4").style.display == "none"){
+    document.getElementById("hid4").style.display = "table";
+    document.getElementById('quizplus2').src = "minus.png";
+    document.getElementById('quizplus2').style.paddingTop = "15px";
+  }
+  else{
+    document.getElementById("hid4").style.display = "none";
+    document.getElementById('quizplus2').src = "plus.png";
+    document.getElementById('quizplus2').style.paddingTop = "0";
+  }
+}
+
+function showMore5(){
+  // if(document.getElementById("hid5").style.display == "none"){
+  //   document.getElementById("hid5").style.display = "table";
+  //   document.getElementById('getset').src = "minus.png";
+  //   document.getElementById('getset').style.paddingTop = "15px";
+  // }
+  // else{
+  //   document.getElementById("hid1").style.display = "none";
+  //   document.getElementById('daplus1').src = "plus.png";
+  //   document.getElementById('daplus1').style.paddingTop = "0";
+  // }
+}
